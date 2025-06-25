f749011875f908648010439d52ca3b81bed9404e
mathi-sri <warshnishreemathi@gmail.com>
2025-06-25 11:21:28 +0530

    Create valid date.c

---
+++ day3/valid date.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int day, month, year;
+    printf("enter the date(dd mm yyyy):");
+    scanf("%d%d%d",&day,&month,&year);
+    
+    if(year >=1 ||)
+
+    return 0;
+}
